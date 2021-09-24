29bd97265c96ebe4e3c5403733d8468049f46ba7
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-24 04:17:17 -0700

    gatsby rebuild

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/src/pages/index.js"))
 }
 
