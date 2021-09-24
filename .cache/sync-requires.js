320e4df60e9953af7cadaa6e3b5d87b430c958d8
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-23 22:33:56 -0700

    fix eslint import error

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/mnt/c/Users/lawbr/lighthouse/lawrence-portfolio/src/pages/index.js"))
+}
+
