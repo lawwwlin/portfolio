b11122ff21e00d7fb07a27540e455a4d6f4f1d53
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-25 15:47:07 -0700

    change links layout in topNav

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -3,10 +3,18 @@ import { Link } from 'gatsby';
 
 const topNav = () => {
   return (
-    <nav className="topNav">
-      <div className="nav-item"><Link className='nav-link' to='/'>HOME</Link></div>
-      <div className="nav-item"><Link className='nav-link' to='/about'>ABOUT</Link></div>
-    </nav>
+    <header>
+      <div className='container'>
+        <div className='logo'>
+          <Link className='nav-link' to='/'>home</Link>
+        </div>
+        <nav className="navigation">
+          <div className="nav-item"><Link className='nav-link' to='/about'>ABOUT</Link></div>
+          <div className="nav-item"><Link className='nav-link' to='/projects'>PROJECTS</Link></div>
+          <div className="nav-item"><Link className='nav-link' to='/contact'>CONTACT</Link></div>
+        </nav>
+      </div>
+    </header>
   )
 };
 
