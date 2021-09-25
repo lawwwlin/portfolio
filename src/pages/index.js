fb5cd93c0bf4ecd1794f0076fe5f53d693751b42
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-24 17:32:26 -0700

    add global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import * as React from "react";
+import * as React from 'react';
 import Layout from '../components/layout';
+import '../styles/global.scss';
 
 const IndexPage = () => {
   return (
