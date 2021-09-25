e7fe36bb3cd9421b36406825f9bd86293f60094e
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-24 17:41:52 -0700

    move all styles sheets to styles folder

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import TopNav from '../components/topNav';
-import '../components/topNav.scss';
+import '../styles/layout.scss';
 
 const Layout = ({ pageTitle, children }) => {
   return (
--- src/components/topNav.js
+++ src/components/topNav.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-import '../components/topNav.scss';
+import '../styles/topNav.scss';
 
 const topNav = () => {
   return (
