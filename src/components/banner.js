c9d020ef5a8253c305347e2e40b71ccaea74d308
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-25 16:40:25 -0700

    add divs for styling

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,25 +1,16 @@
 import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import img from 'gatsby-image';
+// import img from 'gatsby-image';
 
 const Banner = () => {
   return (
-    <header>
+    <div className='banner'>
       <div className='container'>
-        <div className='inner-header'> 
-          <div className='logo'>
-            <Link to='/'>HOME</Link>
-          </div>
-          <div className='navigation'>
-            <nav>
-              <Link to='/about'>ABOUT</Link>
-              <Link to='/projects'>PROJECTS</Link>
-              <Link to='/contact'>CONTACT</Link>
-            </nav>
-          </div>
+        <div className='row'>
+          <div className='main-text'>Lawrence Lin</div>
         </div>
       </div>
-    </header>
+    </div>
   );
 };
 
