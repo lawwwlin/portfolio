f130266b4e1780ddf5cac581cd1e89170256ee74
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-26 02:06:33 -0700

    style change

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 
 // import img from 'gatsby-image';
 
@@ -10,16 +10,16 @@ const Banner = () => {
   return (
     <div className='banner'>
       <div className='fixed-contacts'>
-        <a href='https://github.com/lawwwlin' target='blank'><FontAwesomeIcon icon={faGithubSquare} /></a>
-        <a href='https://www.linkedin.com/in/lawrence-lin-vancouver/' target='blank'><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href='https://resume.creddle.io/resume/6pb2h22k1x1' target='blank'><FontAwesomeIcon icon={faFile} /></a>
+        <a href='https://github.com/lawwwlin' target='blank'><FontAwesomeIcon icon={faGithub} /></a>
+        <a href='https://www.linkedin.com/in/lawrence-lin-vancouver/' target='blank'><FontAwesomeIcon icon={faLinkedinIn} /></a>
+        <a href='https://resume.creddle.io/resume/6pb2h22k1x1' target='blank'><FontAwesomeIcon icon={faFileAlt} /></a>
       </div>
       <div className='container'>
         <div className='row'>
           <div className='main-text'>Lawrence Lin</div>
         </div>
       </div>
-      <div className='fixed-email'><h3>lawwwlin@gmail.com</h3></div>
+      <a href='https://google.ca' target='blank'><div className='fixed-email'><h3>lawwwlin@gmail.com</h3></div></a>
     </div>
   );
 };
