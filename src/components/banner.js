cc53b8b5d927f8826a095f4c5f12ff003d3741f7
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-25 21:35:15 -0700

    add side links to page

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+
 // import img from 'gatsby-image';
 
 const Banner = () => {
   return (
     <div className='banner'>
+      <div className='fixed-contacts'>
+        <FontAwesomeIcon icon={faGithubSquare} />
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faFile} />
+      </div>
       <div className='container'>
         <div className='row'>
           <div className='main-text'>Lawrence Lin</div>
         </div>
       </div>
+      <div className='fixed-email'><h3>lawwwlin@gmail.com</h3></div>
     </div>
   );
 };
