a81306151e30586e18f548d5f9ffb6f2c9923244
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-26 04:04:37 -0700

    style change

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faInfoCircle, faPhoneSquare, faFileCode } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faPhoneSquare, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
@@ -14,7 +14,7 @@ const Header = () => {
           <div className='navigation'>
             <nav>
               <Link to='/about'><FontAwesomeIcon icon={faInfoCircle} /> ABOUT</Link>
-              <Link to='/projects'><FontAwesomeIcon icon={faFileCode} /> PROJECTS</Link>
+              <Link to='/projects'><FontAwesomeIcon icon={faLaptopCode} /> PROJECTS</Link>
               <Link to='/contact'><FontAwesomeIcon icon={faPhoneSquare} /> CONTACT</Link>
             </nav>
           </div>
