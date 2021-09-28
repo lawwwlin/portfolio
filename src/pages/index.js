f3d7f43245e449ef432f14df499a2e1f0a0d22da
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-27 21:58:43 -0700

    add about component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import '../styles/styles.scss';
 
 // Import componenets
 import Header from '../components/header';
-import Banner from '../components/banner.js';
+import Banner from '../components/banner';
+import About from '../components/about';
 
 const IndexPage = () => {
   return (
@@ -11,6 +12,7 @@ const IndexPage = () => {
       <title>Lawrence Lin</title>
       <Header />
       <Banner />
+      <About />
     </div>
   );
 };
