9ea715b9816ffe4957725305f7e9b78b9eef0d20
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-27 21:58:33 -0700

    create about component

---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <p>Full-stack Web Developer, studied basic algorithms and data structures while pursuing an undergraduate degree in Mathematics. In-depth understanding of programming skills including but not limited to: front-end development with React and HTML/CSS, back-end with NodeJS and ​PostgreSQL. Enjoys problem solving and communicating with other developers to enhance programming knowledge. Passionate and eager to learn new technologies and frameworks</p>
+    </div>
+  );
+};
+
+export default About;
