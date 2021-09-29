28fe459fa013ca3fc268b1c0459808c586e3bb55
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-28 20:00:58 -0700

    update content and styles of about component

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,25 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 const About = () => {
   return (
-    <div>
-      <p>Full-stack Web Developer, studied basic algorithms and data structures while pursuing an undergraduate degree in Mathematics. In-depth understanding of programming skills including but not limited to: front-end development with React and HTML/CSS, back-end with NodeJS and ​PostgreSQL. Enjoys problem solving and communicating with other developers to enhance programming knowledge. Passionate and eager to learn new technologies and frameworks</p>
+    <div className="about">
+      <div className="container">
+        <div className="main-text">
+          <div className="text-intro">
+            Full-stack Web Developer, studied basic algorithms and data
+            structures while pursuing an undergraduate degree in Mathematics.
+            In-depth understanding of programming skills including but not
+            limited to: front-end development with React and HTML/CSS, back-end
+            with NodeJS and ​PostgreSQL. Enjoys problem solving and
+            communicating with other developers to enhance programming
+            knowledge. Passionate and eager to learn new technologies and
+            frameworks
+          </div>
+          <div className="text-name"></div>
+          <div className="text-about"></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,27 @@
-import * as React from 'react';
+import * as React from "react";
 
 // Import componenets
-import Header from '../components/header';
+import Header from "../components/header";
 
 const AboutPage = () => {
   return (
-    <div>
-      <Header />
-      <p>Full-stack Web Developer, studied basic algorithms and data structures while pursuing an undergraduate degree in Mathematics. In-depth understanding of programming skills including but not limited to: front-end development with React and HTML/CSS, back-end with NodeJS and ​PostgreSQL. Enjoys problem solving and communicating with other developers to enhance programming knowledge. Passionate and eager to learn new technologies and frameworks</p>
+    <div className="about">
+      <div className="container">
+        <div className="main-text">
+          <div className="text-intro">
+            Full-stack Web Developer, studied basic algorithms and data
+            structures while pursuing an undergraduate degree in Mathematics.
+            In-depth understanding of programming skills including but not
+            limited to: front-end development with React and HTML/CSS, back-end
+            with NodeJS and ​PostgreSQL. Enjoys problem solving and
+            communicating with other developers to enhance programming
+            knowledge. Passionate and eager to learn new technologies and
+            frameworks
+          </div>
+          <div className="text-name"></div>
+          <div className="text-about"></div>
+        </div>
+      </div>
     </div>
   );
 };
