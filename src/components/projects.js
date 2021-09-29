1c7e6c8e9cfc1e16ba8c881d3b720bd1c32b68e4
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-28 20:23:52 -0700

    Create projects.js

---
+++ src/components/projects.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="main-text">
+          <div className="text-intro" id='projects'>
+            projects...
+          </div>
+          <div className="text-name"></div>
+          <div className="text-about"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
