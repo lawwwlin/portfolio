8c9116330fd3af77e0882084d44cce3af6af275f
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-29 20:57:20 -0700

    update about content

--- src/components/about.js
+++ src/components/about.js
@@ -10,15 +10,15 @@ const About = () => {
             About Me
           </div>
           <div className="text-about">
-            I am a full-stack web developer, interested in learning new technologies and finding efficient ways to <br />
-            
-            studied basic algorithms and data structures while pursuing an undergraduate degree in Mathematics.
+            My coding journey started when I was pursuing an undergraduate degree in Mathematics. I had experience with Python, Java and C++ from taking Computer Science courses and exposure to topics such as basic algorithms and data structures.
+            <br /><br />
+            I have strong critical thinking and problem solving skills from my Mathematics background, and after graduation, I pursued my interest in programming by taking the Lighthouse Labs web development bootcamp.
+            <br /><br />
             In-depth understanding of programming skills including but not
             limited to: front-end development with React and HTML/CSS, back-end
             with NodeJS and ​PostgreSQL. Enjoys problem solving and
             communicating with other developers to enhance programming
-            knowledge. Passionate and eager to learn new technologies and
-            frameworks
+            knowledge.
           </div>
         </div>
       </div>
